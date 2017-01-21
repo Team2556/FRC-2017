32a014400370ae8ce7f06f4adf14681f30972d06
andrew-helms <drewdude.h@gmail.com>
2017-01-21 11:10:44 -0600

    Changed sensitivity values

--- src/Commands/Teleoperated/TeleopCommand.cpp
+++ src/Commands/Teleoperated/TeleopCommand.cpp
@@ -17,7 +17,7 @@ void TeleopCommand::Execute(){
 	if(oi->Xbox1.get()->GetYButton())
 		_DriveMode = 1; // Right
 
-	drivetrain.get()->Drive(DeadSens(oi->Xbox1.get()->GetX(frc::XboxController::kLeftHand), 0.025, 0.85), DeadSens(oi->Xbox1.get()->GetY(frc::XboxController::kLeftHand), 0.025, 0.85), DeadSens(oi->Xbox1.get()->GetX(frc::XboxController::kRightHand), 0.025, 0.85), _DriveMode * 90.0);
+	drivetrain.get()->Drive(DeadSens(oi->Xbox1.get()->GetX(frc::XboxController::kLeftHand), 0.025, 0.7), DeadSens(oi->Xbox1.get()->GetY(frc::XboxController::kLeftHand), 0.025, 0.7), DeadSens(oi->Xbox1.get()->GetX(frc::XboxController::kRightHand), 0.025, 0.7), _DriveMode * 90.0);
 }
 
 bool TeleopCommand::IsFinished(){
